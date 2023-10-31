db318a48602e9c42e4b7b87a51c0dff82658f579
John Simeoni <jack.simeoni@gmail.com>
2023-10-31 15:45:40 -0400

    added connection and controllers folders

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const reaction = require('../models/reaction');
+const thought = require('../models/thought');
+const user = require('../models/user');
+
+module.exports = { reaction, thought, user };
